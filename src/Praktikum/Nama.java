da51cf61cc97c71c8c3e44ba475d30d71b01e263
Bayu2 <42332110+Bayu2@users.noreply.github.com>
2018-08-21 12:20:39 +0700

    Add files via upload

---
+++ src/Praktikum/Nama.java
@@ -0,0 +1,270 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class Nama {
+    public static void main(String[] args){
+       {
+        int sp;
+        for (sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        int r1;
+        int ar;
+        for (r1 = 0; r1 < 2; r1++) {
+            System.out.print("PP");
+            for (ar = 0; ar < r1; ar++) {
+                System.out.print("  ");
+            }
+            System.out.println("PP");
+        }
+        for (r1 = 0; r1 < 1; r1++) {
+            System.out.print("PP");
+            for (ar = 0; ar < r1; ar++) {
+                System.out.print("  ");
+            }
+            System.out.println("PP");
+        }
+        for (int p = 0; p < 2; p++) {
+            for (int p1 = 0; p1 < 2; p1++) {
+                System.out.print("P");
+            }
+            System.out.println("");
+ 
+        }
+    } 
+       {
+        int sp;
+        for (sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        int r1;
+        int ar;
+        for (r1 = 0; r1 < 2; r1++) {
+            System.out.print("RR");
+            for (ar = 0; ar < r1; ar++) {
+                System.out.print("  ");
+            }
+            System.out.println("RR");
+        }
+
+        for (r1 = 0; r1 < 3; r1++) {
+            System.out.print("RR");
+            for (ar = 0; ar < r1; ar++) {
+                System.out.print("  ");
+            }
+            System.out.println("RR");
+        }
+    }
+    
+       {
+        for (int sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        for (int sp = 0; sp < 5; sp++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris1
+        for (int sp = 0; sp < 3; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 3; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris2
+        for (int sp = 0; sp < 2; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 3; sp2++) {
+            System.out.print(" A");
+        }
+        System.out.println();//baris3
+        for (int sp = 0; sp < 1; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 7; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris4
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 9; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris5
+    }
+        {
+        int sp;
+        for (sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        for (int t = 0; t < 9; t++) {
+            System.out.print("T");
+        }
+        System.out.println("");
+        for (int s = 0; s < 4; s++) {
+            for (sp = 0; sp < 3; sp++) {
+                System.out.print(" ");
+            }
+            System.out.println("TT");
+        }
+ 
+    }
+        {
+        for (int sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        for (int sp = 0; sp < 5; sp++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris1
+        for (int sp = 0; sp < 3; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 3; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris2
+        for (int sp = 0; sp < 2; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 3; sp2++) {
+            System.out.print(" A");
+        }
+        System.out.println();//baris3
+        for (int sp = 0; sp < 1; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 7; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris4
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 9; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris5
+    }
+        {
+        int sp;
+        for (sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        for (int m = 0; m < 3; m++) {
+            System.out.print("M");
+        }
+        for (int msp = 0; msp < 7; msp++) {
+            System.out.print(" ");
+        }
+        for (int m = 0; m < 3; m++) {
+            System.out.print("M");
+        }
+        System.out.println("");
+        int m1 = 3, m2 = 0, m3 = 0, m4;
+        for (int m = 0; m < 4; m++) {
+            System.out.print("M");
+        }
+        for (int msp = 0; msp < 5; msp++) {
+            System.out.print(" ");
+        }
+        for (int m = 0; m < 4; m++) {
+            System.out.print("M");
+        }
+        System.out.println("");
+        System.out.print("MM");
+        for (m2 = m2; m2 < 1; m2++) { 
+            System.out.print(" "); 
+        } 
+        System.out.print("MM"); 
+        for (m1 = m1; m1 > 0; m1--) {
+            System.out.print(" ");
+        }
+        System.out.print("MM");
+        for (m3 = m3; m3 < 1; m3++) {
+            System.out.print(" ");
+        }
+        System.out.println("MM");
+        System.out.print("MM");
+        for (m2 = 0; m2 < 2; m2++) {
+            System.out.print(" ");
+        }
+        System.out.print("MM");
+        System.out.print(" ");
+        System.out.print("MM");
+        for (m3 = 0; m3 < 2; m3++) {
+            System.out.print(" ");
+        }
+        System.out.println("MM");
+        for (int m = 0; m < 2; m++) {
+            System.out.print("M");
+        }
+        for (int msp = 0; msp < 3; msp++) {
+            System.out.print(" ");
+        }
+        for (int m = 0; m < 3; m++) {
+            System.out.print("M");
+        }
+        for (int msp = 0; msp < 3; msp++) {
+            System.out.print(" ");
+        }
+        for (int m = 0; m < 2; m++) {
+            System.out.print("M");
+        }
+        System.out.println("");
+    }
+        {
+        for (int sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }
+        for (int sp = 0; sp < 5; sp++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris1
+        for (int sp = 0; sp < 3; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 3; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris2
+        for (int sp = 0; sp < 2; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 3; sp2++) {
+            System.out.print(" A");
+        }
+        System.out.println();//baris3
+        for (int sp = 0; sp < 1; sp++) {
+            System.out.print(" ");
+        }
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 7; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris4
+        System.out.print("A");
+        for (int sp2 = 0; sp2 < 9; sp2++) {
+            System.out.print(" ");
+        }
+        System.out.println("A");//baris5
+    }
+ 
+}}
+
+    
+    
+    
